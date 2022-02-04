56e03687bac331bfe257b108a659ace62a8ca1b6
Justin-Ordonez <98716300+Justin-Ordonez@users.noreply.github.com>
2022-02-04 13:32:45 -0500

    Update github_test.c

--- github_test.c
+++ github_test.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+
+struct intercept {
+   int x;
+   int y;
+};
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Github tutorial");
-   printf("test");
-   return 0;
+   
+   // User input points
+   std::array<Point, 3> array = { { 0, 0 }, { 0, 1 }, { 1, 1 } };
+   
+   // Line formula: y = mx + b
+   M = 1;
+   B = 1;
+   
+   // Radius r
+   R = 1;
+   
+   // Calculate intercept points for line and circles
+   
+   
+   
+   // List intercept points in hash table
+   
+   
+   // Sort intercept points (Quick Sort)
+   
+   
+   
+   // Sweep hash table
+   
+   
+   
+   
 }
